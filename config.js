b71cb8366846fe783b9d92ac7b1b5c56cf8df752
jrkasper2 <jrkasper222@gmail.com>
2026-09-21 16:53:14 -0400

    Connect Mirrorball Pool to Supabase

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ window.MIRRORBALL_CONFIG={
   appName:"Mirrorball Pool",
   groupName:"Julia's Mirrorball Pool",
   season:35,
-  supabaseUrl:"",
-  supabaseAnonKey:"",
+  supabaseUrl:"https://ipakrfgvvguxbsiuqlib.supabase.co",
+  supabaseAnonKey:"sb_publishable_3ByMiMHhJ16k6ONVbRIJHA_wjPGQ9fS",
   sourceLinks:{
     officialCast:"https://abc.com/show/f66d60e7-29f2-49cf-85dd-44a11bce42e4",
     castGuide:"https://www.cosmopolitan.com/entertainment/tv/a73477562/dwts-season-35-cast/",
