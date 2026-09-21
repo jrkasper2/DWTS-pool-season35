7405375a7f261f31c04c5d07b81f6015ebef64ae
jrkasper2 <jrkasper222@gmail.com>
2026-09-21 16:42:15 -0400

    Add config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+window.MIRRORBALL_CONFIG={
+  appName:"Mirrorball Pool",
+  groupName:"Julia's Mirrorball Pool",
+  season:35,
+  supabaseUrl:"",
+  supabaseAnonKey:"",
+  sourceLinks:{
+    officialCast:"https://abc.com/show/f66d60e7-29f2-49cf-85dd-44a11bce42e4",
+    castGuide:"https://www.cosmopolitan.com/entertainment/tv/a73477562/dwts-season-35-cast/",
+    scoreSource:"https://parade.com/tv/dancing-with-the-stars",
+    recapSource:"https://ew.com/tv/dancing-with-the-stars/"
+  }
+};
